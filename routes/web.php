8f0357675000e565eab52b7b20df5945597f9baf
hsemix <semix.hamidouh@gmail.com>
2018-10-30 00:11:46 +0300

    Added return types to routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
 
 Route::get('/', function() {
-    return view(new App\ViewModels\Welcome('Welcome to Yuga Framework'));
+    return new App\ViewModels\Welcome('Welcome to Yuga Framework');
 });
