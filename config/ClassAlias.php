43dbf2120df9d34eee017f6cc7fb9573b77f87c9
Hamid Semix <semix.hamidouh@gmail.com>
2019-11-04 11:26:16 +0300

    removed duplicates

--- config/ClassAlias.php
+++ config/ClassAlias.php
@@ -8,7 +8,6 @@
 return [
     'DB'                    => \Yuga\Database\Query\DB::class,
     'Route'                 => \Yuga\Route\Route::class,
-    'PermanentDeleteTrait'  => '\Yuga\Database\Elegant\Traits\PermanentDeleteTrait',
     'PermanentDeleteTrait'  => \Yuga\Database\Elegant\Traits\PermanentDeleteTrait::class,
     'App'                   => \Yuga\App::class,
     'Request'               => \Yuga\Http\Request::class,
@@ -18,9 +17,9 @@ return [
     'Redirect'              => \Yuga\Http\Redirect::class,
     'NotFoundHttpException' => \Yuga\Route\Exceptions\NotFoundHttpException::class,
     'Storage'               => \Yuga\Logger\Storage::class,
-    'Str'                   => Yuga\Support\Str::class,
-    'FileSystem'            => Yuga\Support\FileSystem::class,
-    'Config'                => Yuga\Support\Config::class,
-    'Inflect'               => Yuga\Support\Inflect::class,
-    'Image'                 => Yuga\Support\Image::class,
+    'Str'                   => \Yuga\Support\Str::class,
+    'FileSystem'            => \Yuga\Support\FileSystem::class,
+    'Config'                => \Yuga\Support\Config::class,
+    'Inflect'               => \Yuga\Support\Inflect::class,
+    'Image'                 => \Yuga\Support\Image::class,
 ];
